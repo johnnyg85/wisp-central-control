ce81be9eee617bb9fb534041d9c97bc061e3934b
Vivek Dinesh <vivekdinesh5@gmail.com>
2015-09-22 15:57:46 +0530

    Added bootstrap styling for subscriptions form.

--- packages/mdisc:stripe/client/templates/subscription/stripe-payment-form.js
+++ packages/mdisc:stripe/client/templates/subscription/stripe-payment-form.js
@@ -4,6 +4,30 @@ Template.stripePaymentForm.helpers({
     },
     formatAmount: function (amount) {
         return "$" + (amount / 100);
+    },
+    getMonths: function() {
+        return [
+            {name: "Jan", val: "01"},
+            {name: "Feb", val: "02"},
+            {name: "Mar", val: "03"},
+            {name: "Apr", val: "04"},
+            {name: "May", val: "05"},
+            {name: "Jun", val: "06"},
+            {name: "Jul", val: "07"},
+            {name: "Aug", val: "08"},
+            {name: "Sep", val: "09"},
+            {name: "Oct", val: "10"},
+            {name: "Nov", val: "11"},
+            {name: "Dec", val: "12"}
+        ];
+    },
+    getYears: function() {
+        var d = new Date();
+        var years = new Array();
+        for (var i=0, year=d.getFullYear(); i<=30; i++,year++) {
+            years.push({"year": year});
+        }
+        return years;
     }
 });
 
@@ -11,6 +35,8 @@ Template.stripePaymentForm.events({
     'submit #payment-form': function (e) {
         e.preventDefault();
         $(e.target).find('button').prop('disabled', true);
+        $('#payment-form').find('.payment-errors').text('');
+        $('#payment-form').find('.payment-errors').css("display", "none");
         Stripe.card.createToken($(e.target), stripeResponseHandler);
     }
 });
@@ -19,6 +45,7 @@ var stripeResponseHandler = function StripeResponseHandler(status, response) {
     var $form = $('#payment-form');
     if (response.error) {
         $form.find('.payment-errors').text(response.error.message);
+        $form.find('.payment-errors').css("display", "block");
         $form.find('button').prop('disabled', false);
     } else {
         var token = response.id;
@@ -28,9 +55,11 @@ var stripeResponseHandler = function StripeResponseHandler(status, response) {
             if (error) {
                 console.log(error.reason);
                 $form.find('.payment-errors').text(error.reason);
+                $form.find('.payment-errors').css("display", "block");
                 $form.find('button').prop('disabled', false);
             } else {
                 $form.find('.payment-errors').text('');
+                $form.find('.payment-errors').css("display", "none");
                 alert('Payment complete.');
             }
         });
