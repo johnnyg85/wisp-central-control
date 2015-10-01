58b09df56d9dce7e0fb05fec481ee76057cc02b3
Spencer Lambert <spence@wisptools.com>
2015-10-01 10:10:08 -0600

    adding more template tests

--- packages/mdisc:mandrill/client/templates/test.js
+++ packages/mdisc:mandrill/client/templates/test.js
@@ -9,5 +9,49 @@ Template.mdMandrillTest.events({
     } else {
       console.log('email address not set');
     }
-  }
+  },
+  'click a#btn-payment': function(e, t) {
+    e.preventDefault();
+
+    var email = document.getElementById('email').value;
+    if (email != '') {
+      Meteor.call('sendEmail','payment-receipt',email);
+      console.log('payment-receipt email sent');
+    } else {
+      console.log('email address not set');
+    }
+  },
+  'click a#btn-data': function(e, t) {
+    e.preventDefault();
+
+    var email = document.getElementById('email').value;
+    if (email != '') {
+      Meteor.call('sendEmail','concierge-data-received',email);
+      console.log('concierge-data-received email sent');
+    } else {
+      console.log('email address not set');
+    }
+  },
+  'click a#btn-ship': function(e, t) {
+    e.preventDefault();
+
+    var email = document.getElementById('email').value;
+    if (email != '') {
+      Meteor.call('sendEmail','concierge-order-shipped',email);
+      console.log('concierge-order-shipped email sent');
+    } else {
+      console.log('email address not set');
+    }
+  },
+  'click a#btn-pass': function(e, t) {
+    e.preventDefault();
+
+    var email = document.getElementById('email').value;
+    if (email != '') {
+      Meteor.call('sendEmail','forgot-password',email);
+      console.log('forgot-password email sent');
+    } else {
+      console.log('email address not set');
+    }
+  },
 });
