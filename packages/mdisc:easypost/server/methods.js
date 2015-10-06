f42120fa276aa5108c7eb40bd65a3b9548e37b4f
Sobhith <sobhith@wisptools.com>
2015-10-06 13:39:02 +0530

    added modified methods for easy post package

--- packages/mdisc:easypost/server/methods.js
+++ packages/mdisc:easypost/server/methods.js
@@ -1,80 +1,56 @@
-var easypost =Easypost(Meteor.settings.easypost.apiKey);
+var easypost = Easypost(Meteor.settings.easypost.apiKey);
 Future = Npm.require('fibers/future');
-//console.log(easypost);
 // verify address
 Meteor.methods({
-
-mdEasypostVerifyAddress: function(fromAddress) {
-console.log("hi");
-var future = new Future();
-easypost.Address.create(fromAddress, function(err, fromAddress) {
-fromAddress.verify(function(err, response) {
-var verifiedAddress;
-if (err) {
-console.log('Address is invalid.');
-future.return( {error:'Address is invalid'} );
-} else if (response.message !== undefined && response.message
-!== null) {
-console.log('Address is valid but has an issue: ',
-response.message);
-verifiedAddress = response.address;
-} else {
-verifiedAddress = response;
-console.log(verifiedAddress);
-}
-future.return(verifiedAddress);
-});
-});
-return future.wait();
-},
-  /*  mdEasypostVerifyAddress: function(fromAddress) {
-        console.log("hi");
-        var myFuture1 = new Future();
+    mdEasypostVerifyAddress: function(fromAddress) {
+      
+        var future = new Future();
         easypost.Address.create(fromAddress, function(err, fromAddress) {
-            var myFuture2 = new Future();
             fromAddress.verify(function(err, response) {
+                var verifiedAddress;
                 if (err) {
                     console.log('Address is invalid.');
-                } else if (response.message !== undefined && response.message !== null) {
-                    console.log('Address is valid but has an issue: ', response.message);
-                    var verifiedAddress = response.address;
+                    future.return({error: 'Address is invalid'});
+                } else if (response.message !== undefined && response.message
+                        !== null) {
+                    console.log('Address is valid but has an issue: ',
+                            response.message);
+                    verifiedAddress = response.address;
                 } else {
-                    var verifiedAddress = response;
+                    verifiedAddress = response;
                     console.log(verifiedAddress);
-                     myFuture2.return(verifiedAddress);
                 }
+                future.return(verifiedAddress);
             });
-            myFuture1.return(myFuture2.wait());
         });
-        return myFuture1.wait();
-           
+        return future.wait();
+    },
+    // set parcel
 
-},*/
-// set parcel
-
-mdEasypostSetParsel:function(){
-easypost.Parcel.create({
-predefined_package: "InvalidPackageName",
-weight: 21.2
-}, function(err, response) {
-console.log(err);
-});
-},
-
-// create shipment
-mdEasypostCreateShipment:function(toAddress,fromAddress,parcel)
-{
-easypost.Shipment.create({
-to_address: toAddress,
-from_address: fromAddress,
-parcel: parcel
-}, function(err, shipment) {
-// shipment.lowestRate filters by carrier name and service name, and accepts negative filters by preceding the name with an exclamation mark
-shipment.buy({rate: shipment.lowestRate(['USPS'], '!LibraryMail, !mediaMAIL')}, function(err, shipment) {
-console.log(shipment);
-
-});
-return shipment;
-});
-}
+    mdEasypostSetParsel: function() {
+        easypost.Parcel.create({
+            predefined_package: "InvalidPackageName",
+            weight: 21.2
+        }, function(err, response) {
+            console.log(err);
+        });
+    },
+    // create shipment
+    mdEasypostCreateShipment: function(toAddress, fromAddress, parcel)
+    {
+        var future = new Future();
+        easypost.Shipment.create({
+            to_address: toAddress,
+            from_address: fromAddress,
+            parcel: parcel
+        }, function(err, shipment) {
+        // shipment.lowestRate filters by carrier name and service name, and accepts negative filters 
+        // by preceding the name with an exclamation mark
+            shipment.buy({rate: shipment.lowestRate(['USPS'], '!LibraryMail, !mediaMAIL')}, function(err, response) {
+                var shippmentlabel = response.postage_label.label_url;
+                future.return(shippmentlabel);
+            });
+        });
+        return future.wait();
+    }
 });
