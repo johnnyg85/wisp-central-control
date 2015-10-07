e2d319b8eceea22fa9f02a781a2d27be93b95d65
Sobhith <sobhith@wisptools.com>
2015-10-07 13:55:50 +0530

    Added menu

--- packages/mdisc:easypost/client/menu.js
+++ packages/mdisc:easypost/client/menu.js
@@ -1,2 +1,2 @@
 WtMenu.addDropdown('Tests', 'fa-exclamation', 0);
-WtMenu.addDropdownItem('Tests', 'Shiping Label', 'mdEasyPost', 'fa-file-text-o', 0);
+WtMenu.addDropdownItem('Tests', 'Shiping Label', 'CreateShippingLabel', 'fa-file-text-o', 0);
