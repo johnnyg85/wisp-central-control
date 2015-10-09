461d32d73de99b0cb1305eb6ae26af7d0e43599b
Vivek Dinesh <vivek@wisptools.com>
2015-10-09 14:30:55 +0530

    Added the line and box-overlay in barcode scanner (Not in the info page scanner)

--- packages/mdisc:shipping/client/templates/shipping_scan.js
+++ packages/mdisc:shipping/client/templates/shipping_scan.js
@@ -58,6 +58,29 @@ Template.mdShippingScan.onRendered(function() {
     Quagga.init(config, function (err) {
         Quagga.start();
     });
+    Quagga.onProcessed(function (result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay;
+        var drawingCanvas = Quagga.canvas.dom.overlay;
+        
+        if (result) {
+            if (result.boxes) {
+                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                result.boxes.filter(function (box) {
+                    return box !== result.box;
+                }).forEach(function (box) {
+                    Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+                });
+            }
+            
+            if (result.box) {
+                Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+            }
+            
+            if (result.codeResult && result.codeResult.code) {
+                Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+            }
+        }
+    });
     var lastScanned = "";
     Quagga.onDetected(function (result) {
         var code = result.codeResult.code;
