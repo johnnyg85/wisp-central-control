93dfe9456667c77ae344ad5ea348735c5e0eb694
Sobhith <sobhith@wisptools.com>
2015-10-13 13:14:03 +0530

    added track shipment menu

--- packages/mdisc:easypost/client/menu.js
+++ packages/mdisc:easypost/client/menu.js
@@ -1,2 +1,3 @@
 WtMenu.addDropdown('Tests', 'fa-exclamation', 0);
 WtMenu.addDropdownItem('Tests', 'Shiping Label', 'CreateShippingLabel', 'fa-file-text-o', 0);
+WtMenu.addDropdownItem('Tests', 'Track Shipment', 'mdTrackShipping', 'fa-file-text-o', 0);
