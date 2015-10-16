3b43eb54f14a71503942c78e7f8c0392bf7a97e2
Spencer Lambert <spence@wisptools.com>
2015-10-15 19:52:50 -0600

    adding move to NAS job type, fixing job security

--- packages/mdisc:jobs/lib/jobs.js
+++ packages/mdisc:jobs/lib/jobs.js
@@ -1,12 +1,16 @@
 
-MdJobs = JobCollection('md_jobs');
+MdJobs = {
+  jc:     JobCollection('md_jobs'),
+  paused: WtCollection('md_jobs_paused')
+};
 
 if (Meteor.isServer) {
-  MdJobs.allow({
+  MdJobs.jc.allow({
     // Grant full permission to any authenticated user
     // ToDo: only allow admin role users this access level.
     admin: function (userId, method, params) {
-      return (userId ? true : false);
+      if (Roles.userIsInRole(userId, ['admin'])) return true;  // is admin
+      return false; // everyone else;
     }
   });
 
@@ -14,26 +18,42 @@ if (Meteor.isServer) {
     // Normal Meteor publish call, the server always
     // controls what each client can see
     Meteor.publish('allJobs', function () {
-      return MdJobs.find({});
+      if (!this.userId) return false; // Not logged in
+      if (Roles.userIsInRole(this.userId, ['admin'])) return MdJobs.jc.find({});  // is admin
+      return false; // everyone else;
     });
 
     // Start the queue running
-    return MdJobs.startJobServer();
+    return MdJobs.jc.startJobServer();
   });
 
   Meteor.methods({
     downloadArchive: function (archiveId) {
-      var job = new Job(MdJobs, 'downloadArchive', 
+      var job = new Job(MdJobs.jc, 'downloadArchive', 
         {
           archiveId: archiveId
         }
       );
       job.priority('normal').retry({retries: 5, wait: 5*60*1000}).save();
+    },
+    moveArchiveToNAS: function (server, data) {
+      var job = new Job(MdJobs.jc, 'moveArchiveToNAS-' + server, data);
+      job.priority('normal').retry({retries: 5, wait: 5*60*1000}).save();
+      //TODO: check if moveArchiveToNAS is paused
+    },
+    pauseArchiveToNAS: function () {
+      MdJobs.paused.update({jobType: 'moveArchiveToNAS'}, {$set: {paused: true}}, {upsert: true});
+      //TODO: find all type moveArchiveToNAS jobs that are waiting or ready then call MdJobs.jc.pauseJobs()
+    },
+    resumeArchiveToNAS: function () {
+      MdJobs.paused.update({jobType: 'moveArchiveToNAS'}, {$set: {paused: true}}, {upsert: false});
+      //TODO: find all type moveArchiveToNAS jobs that are waiting or ready then call MdJobs.jc.resumeJobs()
     }
   });
 
+  // Not implemented at this time...
   MdJobs.initAutoCloudArchive = function (archiveId) {
-      var job = new Job(MdJobs, 'initAutoCloudArchive', 
+      var job = new Job(MdJobs.jc, 'initAutoCloudArchive', 
         {
           archiveId: archiveId
         }
@@ -43,7 +63,6 @@ if (Meteor.isServer) {
 }
 
 if (Meteor.isClient) {
-
   Meteor.startup(function () {
     Meteor.subscribe('allJobs');
   });
