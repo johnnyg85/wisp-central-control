70cbc4ef20a0a9540d8df424a7c376db9f514d1d
Sobhith <sobhith@wisptools.com>
2015-10-27 13:48:38 +0530

    add router for tracking

--- packages/mdisc:manage-account/lib/router.js
+++ packages/mdisc:manage-account/lib/router.js
@@ -2,3 +2,17 @@ Router.route('/my-account/', {
   name: 'mdMyAccount', 
   template: 'mdMyAccount'
 });
+
+Router.route('/Track-Shipments/:trackid', {
+    name:'mdTrackShipment',
+    template:'mdTrackShipping',
+    data: function(){
+       
+         var trackCode = this.params.trackid;
+         console.log(trackCode);
+         return trackCode;
+         
+    }
+    
+
+});
