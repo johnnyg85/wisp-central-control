6be2e709480914176c169fa81d9727e2ae43e4dd
Sobhith <sobhith@wisptools.com>
2015-10-30 13:37:36 +0530

    Changes to the router

--- packages/mdisc:manage-account/lib/router.js
+++ packages/mdisc:manage-account/lib/router.js
@@ -2,17 +2,3 @@ Router.route('/my-account/', {
   name: 'mdMyAccount', 
   template: 'mdMyAccount'
 });
-
-Router.route('/Track-Shipments/:trackid', {
-    name:'mdTrackShipment',
-    template:'mdTrackShipping',
-    data: function(){
-       
-         var trackCode = this.params.trackid;
-         console.log(trackCode);
-         return trackCode;
-         
-    }
-    
-
-});
