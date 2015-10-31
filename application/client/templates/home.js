e8dcfda49c972ea071ebc58bcb6e41af53dde2ec
Spencer Lambert <spence@wisptools.com>
2015-10-30 19:47:56 -0600

    Improving style

--- application/client/templates/home.js
+++ application/client/templates/home.js
@@ -1,32 +1,5 @@
 Template.home.onRendered(function() {
 
-    // Fade nav bar
-
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > 0) {
-            $('nav').addClass('fixed');
-        } else {
-            $('nav').removeClass('fixed');
-        }
-    });
-
-    // Close mobile menu once link is clicked
-    
-    $('.menu li a').click(function(){
-      if($('nav').hasClass('nav-open')){
-        $('nav').removeClass('nav-open');
-      }
-    });
-
-    // Mobile nav
-
-    $('.mobile-toggle').click(function() {
-        $(this).closest('nav').toggleClass('nav-open');
-        if ($(this).closest('nav').hasClass('nav-3')) {
-            $(this).toggleClass('active');
-        }
-    });
-
     // Append .background-image-holder <img>'s as CSS backgrounds
 
     $('.background-image-holder').each(function() {
