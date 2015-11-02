d9f426ac3c1b8885b758fe1ebb9771c080ee9a1f
Spencer Lambert <spence@wisptools.com>
2015-11-02 13:37:56 -0700

    adding login check prior to archive create

--- packages/mdisc:manage-account/lib/router.js
+++ packages/mdisc:manage-account/lib/router.js
@@ -1,4 +1,18 @@
 Router.route('/account/', {
   name: 'mdMyAccount', 
-  template: 'mdMyAccount'
+  template: 'mdMyAccount',
+  onBeforeAction: function () {
+    if (Session.get('afterLoginDoArchive')) {
+      Session.set('afterLoginDoArchive', false); // clear session var
+
+      // Simulate clicking the archive now button
+      var e = $.Event('click');
+      $('#authorizeGooglePhoto').trigger(e);
+
+      // render home
+      this.render('home');
+    } else {
+      this.next();
+    }
+  }
 });
