40fa91ccd1aac4b864f88923ab8e58bfe74eb31e
Spencer Lambert <spence@wisptools.com>
2015-11-26 18:51:59 -0700

    adding delaySubscriptionCheck

--- packages/mdisc:jobs-subscriptions/server/methods.js
+++ packages/mdisc:jobs-subscriptions/server/methods.js
@@ -1,15 +1,22 @@
   Meteor.methods({
     monitorSubscription: function () {
       var userId = Meteor.userId();
-      var date = new Date();
-      date.setDate(date.getDate() + 25); // Start checking 25 days from now. The first archive is scheduled when order placed.
       var job = new Job(MdJobsSubscriptions.jc, 'monitorSubscription', 
         {
           userId: userId
         }
       );
       job.priority('normal').repeat({
-          schedule: MdJobsSubscriptions.jc.later.parse.text('every day')
-      }).after(date).save();
+          schedule: MdJobsSubscriptions.jc.later.parse.text('every 1 minute')
+      }).save();
+    },
+    delaySubscriptionCheck: function (jobId, days) {
+        var job = MdJobsSubscriptions.jc.getJob(jobId);
+        if (job) {
+          var date = new Date();
+          date.setDate(date.getDate() + days); // Start checking again after 25 days.
+          job.pause();
+          job.after(date).save();
+        }
     }
   });
