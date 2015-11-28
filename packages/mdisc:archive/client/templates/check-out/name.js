9414405cc1645714a3ba9308899b5aae4859a129
Spencer Lambert <spence@wisptools.com>
2015-11-27 19:05:21 -0700

    removing test data

--- packages/mdisc:archive/client/templates/check-out/name.js
+++ packages/mdisc:archive/client/templates/check-out/name.js
@@ -14,8 +14,7 @@ Template.mdArchiveName.events({
       var data = {
         archiveName: e.target[0].value,
         active: false,
-        version: '0.0.1',
-        availableArchives: 1
+        version: '0.0.1'
       }
       MdArchive.subscription.insert(data);
     }
