1b91c193670d76743a4e2564f1f88c3e0b049635
Spencer Lambert <spence@wisptools.com>
2015-11-30 15:15:17 -0700

    adding cpu profiler package for kadira

--- packages/mdisc:archive/client/templates/check-out/name.js
+++ packages/mdisc:archive/client/templates/check-out/name.js
@@ -14,7 +14,8 @@ Template.mdArchiveName.events({
       var data = {
         archiveName: e.target[0].value,
         active: false,
-        version: '0.0.1'
+        version: '0.0.1',
+        subscriptionPlan: 'annual'
       }
       MdArchive.subscription.insert(data);
     }
