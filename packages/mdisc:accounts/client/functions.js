c31da0e9479d2ce90d380989865253d73f2095e0
Spencer Lambert <spence@wisptools.com>
2015-12-06 07:53:41 -0700

    adding profile and email to google scope

--- packages/mdisc:accounts/client/functions.js
+++ packages/mdisc:accounts/client/functions.js
@@ -2,7 +2,7 @@ MdAccounts = {
   signIn: function (redirectUrl) {
     Meteor.loginWithGoogle({
       // Must include Google Photos access everytime, or else it is dropped on subsequent logins.
-      requestPermissions: ['https://picasaweb.google.com/data/'],
+      requestPermissions: ['profile','email','https://picasaweb.google.com/data/'],
       requestOfflineToken: 'true',
       loginStyle: 'redirect', 
       redirectUrl: redirectUrl
